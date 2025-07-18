69cbb5b36c9c60be232ea69b7d0145e9be2758fe
AnuragMali333 <anuragmali654@gmail.com>
2025-07-18 12:33:02 +0530

    added comments

--- Arrays/nextGreatestEle.java
+++ Arrays/nextGreatestEle.java
@@ -1,6 +1,6 @@
 public class nextGreatestEle {
-  public static void main(String[] args) {
-    int[]arr={12,8,41,37,2,49,16,28,21};
+  public static void main(String[] args) { // Optimized approach. 
+    int[]arr={12,8,60,37,2,49,16,28,21};
     int i=arr.length-2;
     int max =arr[arr.length-1]; //21
     arr[arr.length-1]=-1; // set last ele as -1
